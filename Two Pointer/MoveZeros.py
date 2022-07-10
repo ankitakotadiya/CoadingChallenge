16ce59325777169e432f5bec5f71856a1da593b4
ankitakotadiya <ankitakotadita@gmail.com>
2022-07-10 08:46:10 +0100

    Create MoveZeros.py

---
+++ Two Pointer/MoveZeros.py
@@ -0,0 +1,14 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        
+        p = 0
+        
+        for i in range(len(nums)):
+            
+            if nums[i] != 0:
+                
+                if i != p:
+                    
+                    nums[i],nums[p] = nums[p],nums[i]
+            
+                p+=1
