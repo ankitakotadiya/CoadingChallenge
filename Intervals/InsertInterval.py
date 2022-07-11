d3552ee348f2cd049c30d27b71aa4e6632764b97
ankitakotadiya <ankitakotadita@gmail.com>
2022-07-11 13:46:44 +0100

    Create InsertInterval.py

---
+++ Intervals/InsertInterval.py
@@ -0,0 +1,17 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        i, j = 0, 1
+        intervals.append(newInterval)
+        intervals.sort()
+        
+        while j < len(intervals):
+            
+            if intervals[i][1] >= intervals[j][0]:
+                intervals[i][1] = max(intervals[i][1], intervals[j][1])
+                s = intervals.pop(j)
+            else: 
+                i+=1
+                j+=1
+        
+        return intervals
