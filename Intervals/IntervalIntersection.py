d95fe80c1f3d86c01fbf8ace5d7e6bb32be53c24
ankitakotadiya <ankitakotadita@gmail.com>
2022-07-11 09:05:19 +0100

    Create IntervalIntersection.py

---
+++ Intervals/IntervalIntersection.py
@@ -0,0 +1,24 @@
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        
+        i , j = 0,0
+        result = []
+        
+        while i < len(firstList) and j < len(secondList):
+            si,ei = firstList[i]
+            sj,ej = secondList[j]
+            
+            if si <= ej and sj <= ei:
+                result.append((max(si,sj),min(ei,ej)))
+                
+            if ei > ej:
+                j+=1
+            
+            elif ei < ej:
+                i+=1
+            
+            else:
+                i+=1
+                j+=1
+                
+        return result
