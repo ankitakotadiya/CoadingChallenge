a3e5da62ab1e967631752e03746b8a79f8c761c8
ankitakotadiya <ankitakotadita@gmail.com>
2022-08-17 13:58:15 +0100

    Description
    # Sort the given array.
    # Loop over the array and fix the first element.
    # now fix the left pointer at i+1 position and right pointer at n-1 position when n is the length of an array.
    # Look at the sum of current element, element at left placed and element at right placed.
    # if sum is greater than target then decrease the right pointer to reduce the sum.
    # if sum is smaller than target then increase the left pointer to increase the sum.
    # at one point if sum is equal to the target then store that triples into your resultant array and increment left pointer by one in order to find another triples. Continue the same process till left and right pointer meet at the same position.
    # Now, fix the second element and shift left and right pointer until iterate all the elements and keep storing triples where condition match the requirement. So at the end you will get all the possible triples.
    
    Time complexity: O(nlogn + n^2)
    Space Complexity: O(1)

--- Two Pointer/3Sum.py
+++ Two Pointer/3Sum.py
@@ -26,6 +26,6 @@ class Solution:
                     l+=1
                     
                     while nums[l] == nums[l-1] and l<r:
-                        l+=1
+                        l += 1
                     
         return res
