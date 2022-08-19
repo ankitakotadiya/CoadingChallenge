caf095f15e56465d294297e5a9643daf57907a27
ankitakotadiya <ankitakotadita@gmail.com>
2022-08-19 09:52:47 +0100

    Update MeetingRoom.py

--- Intervals/MeetingRoom.py
+++ Intervals/MeetingRoom.py
@@ -1,18 +1,35 @@
+'''
+# First sort the invervals.
+# Intially assign room = 1 because for first interval you need one room.
+# Now check interval 1 and interval two, if current start is less than the previous interval's end that means there is overlap and you need a separate room so increase room count by one.
+# Run through loop and at the end return number of rooms required.
+
+Time Complexity: O(nLogn)
+Space Complexity: O(1)
+'''
+
+'''
+Number of rooms required to attend all the meetings.
+Input: [[0, 30],[5, 10],[15, 20]]
+Output: 2
+'''
+
 class Solution:
   def meetingRoom(self,intervals):
     
     intervals.sort()
     
-    i,j = 0,1
     room = 1
-    while(j < len(intervals):
-          
-          if intervals[i][0] < intervals[j][1] and intervals[j][0] < intervals[i][1]:
-            room+=2
-          else:
-            i+=1
-            j+=1
-          
+    lastend = intervals[0][1]
+
+    for i in range(1,len(intervals)):
+      start,end = intervals[i]
+      
+      if lastend > start:
+        room += 1
+        
+      lastend = end
+
     return room
             
             
