ce7b0a60c09b3d55baa45a72dd52a0ed36eb668d
ankitakotadiya <ankitakotadita@gmail.com>
2022-08-19 08:50:23 +0100

    Update InsertInterval.py

--- Intervals/InsertInterval.py
+++ Intervals/InsertInterval.py
@@ -1,17 +1,27 @@
+'''
+'''
+
+'''
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+'''
+
 class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         
-        i, j = 0, 1
         intervals.append(newInterval)
         intervals.sort()
+        
+        i,j = 0,1
         
         while j < len(intervals):
             
             if intervals[i][1] >= intervals[j][0]:
-                intervals[i][1] = max(intervals[i][1], intervals[j][1])
-                s = intervals.pop(j)
-            else: 
+                intervals[i][1] = max(intervals[i][1],intervals[j][1])
+                intervals.pop(j)
+                
+            else:
                 i+=1
                 j+=1
-        
+                
         return intervals
