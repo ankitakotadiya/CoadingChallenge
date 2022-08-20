2d0e4bd21984880f0dc781ecfb414c3befb4c6cd
ankitakotadiya <ankitakotadita@gmail.com>
2022-08-20 09:24:28 +0100

    Update SquareRoot.py

--- Search/SquareRoot.py
+++ Search/SquareRoot.py
@@ -1,3 +1,23 @@
+'''
+# The approach is binary search. 
+# Set left point equal to 0 and right pointer qual to x.
+# Find out mid point and calculate square of mid point.
+# If square of mid point is equal to the given number then return mid is the our answer.
+# If squre is greater than x then discard right part because square should not be greater than num so we need to search in left part only.
+# If less than then discard left part and will continue searach in right part until we find square root.
+
+Time Complexity: O(logn)
+Space Complexity: O(1)
+'''
+
+'''
+Given a non-negative integer x, compute and return the square root of x.
+
+Input: x = 4
+Output: 2
+'''
+
+
 class Solution:
     def mySqrt(self, x: int) -> int:
         
