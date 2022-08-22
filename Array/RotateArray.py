925ce63eadad7a03f37cf74a5cc44153d8fc769e
ankitakotadiya <ankitakotadita@gmail.com>
2022-08-22 13:42:48 +0100

    Update RotateArray.py

--- Array/RotateArray.py
+++ Array/RotateArray.py
@@ -1,3 +1,23 @@
+'''
+# In this problem rotate last kth element.
+# We run the loop from 0 to kth element and each and every iteration pop element from array insert at 0 index.
+# At the end of loop you would get rotated array
+
+Time Complexity: O(k)
+Space Complexity: O(1)
+'''
+
+'''
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+nput: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+'''
+
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         
