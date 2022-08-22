356fff1863c0f13408ac9262be7697072362b5f3
ankitakotadiya <ankitakotadita@gmail.com>
2022-08-22 13:31:18 +0100

    Update MaximumProductofThreeNum.py

--- Array/MaximumProductofThreeNum.py
+++ Array/MaximumProductofThreeNum.py
@@ -1,3 +1,19 @@
+'''
+# The array is sorted so simply product of last three number is our answer.
+# We need to check that that is last three values are negative then would get negative product, in such case will check if product of first three values are greater than the last three then return product of first three.
+# Else would return product of last three values.
+
+Time Complexity: O(1)
+Space Complexity: O(1)
+'''
+
+'''
+Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+
+Input: nums = [1,2,3,4]
+Output: 24
+'''
+
 class Solution:
     def maximumProduct(self, nums: List[int]) -> int:
         
