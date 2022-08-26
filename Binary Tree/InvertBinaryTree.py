08e5623412b3ed92a78a13084cee465a42857cff
ankitakotadiya <ankitakotadita@gmail.com>
2022-08-26 08:01:36 +0100

    Create InvertBinaryTree.py

---
+++ Binary Tree/InvertBinaryTree.py
@@ -0,0 +1,35 @@
+'''
+Given the root of a binary tree, invert the tree, and return its root.
+
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+'''
+
+'''
+# Invert binary tree means we need to convert mirror image of tree. I will use DFS.
+# Recursively travers left and right node and swap them.
+# Follow same process for all left and right nodes.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+'''
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        
+        if root is None:
+            return
+        
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+        
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        
+        return root
