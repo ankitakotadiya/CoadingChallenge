a869cd1d27c105bfbbac7ab8cff8a267418d6aa8
ankitakotadiya <ankitakotadita@gmail.com>
2022-09-05 07:44:13 +0100

    Update GeneratePowersetKsize.py

--- Recursion/GeneratePowersetKsize.py
+++ Recursion/GeneratePowersetKsize.py
@@ -8,24 +8,23 @@ Write a program which computes all size k subsets of 11,,2,...,?tl, where k and 
 # If not then first compute k size subset of [2...n] if contain 1 then we compute k-1 size subset of 2 to n and simply add 1 to each of them.
 # Final answer will get if I take as an example 1,2,3 then {2,3},{1,3},{1,2}
 
-Time Complexity: n*(n^k)
+Time Complexity: O(n*(2^k))
 Space Complexity: O(n)
 '''
 
-def generate_power_set_k(n,k):
-  
-  def directed_power_set(offset,combination):
-    
-    if len(combination) == k:
-      result.append(list(combination))
-      return
-    
-    num_remaining = k - len(combination)
-    i = offset
-    while i <= n and num_remaining <= n - i + 1:
-      directed_power_set(i+1,combination + [i])
-      i+=1
-    
-  result = []
-  directed_power_set(1,[])
-  return result
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        
+        result = []
+        def directed_combine(nex,path):
+            
+            if len(path) == k:
+                result.append(path)
+                return 
+            
+            for i in range(nex,n+1):
+                
+                directed_combine(i+1,path+[i])
+                
+        directed_combine(1,[])
+        return result
