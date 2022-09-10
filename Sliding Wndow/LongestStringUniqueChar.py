e290a6b51bbb52abc01f6f9d4dba21e8b12bbf59
ankitakotadiya <ankitakotadita@gmail.com>
2022-09-10 09:27:36 +0100

    Update LongestStringUniqueChar.py

--- Sliding Wndow/LongestStringUniqueChar.py
+++ Sliding Wndow/LongestStringUniqueChar.py
@@ -1,3 +1,15 @@
+'''
+# Initialise left and right pointer equal to 0.
+# Initialise an array to store the characters of the current window. This array always store unique characters.
+# Move right pointer towards N, where N is the of the array.
+# Till this right point we have maximum non repeating substring length. Store that length of substring to the result in each iteration.
+# If a character is found, which is present in the array then remove that character and append current character.
+# Follow the process to the end of the character and return maximum size of substring.
+
+Time Complexity: O(n)
+Space Complexity: O(m), where m is length of the unique character.
+'''
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         
