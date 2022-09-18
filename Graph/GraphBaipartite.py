716332704898c0392918b15dd08777b43478d3b1
ankitakotadiya <ankitakotadita@gmail.com>
2022-09-18 15:52:04 +0100

    Update GraphBaipartite.py

--- Graph/GraphBaipartite.py
+++ Graph/GraphBaipartite.py
@@ -1,6 +1,34 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         
+        colors = [None]*len(graph)
+        que = []
+        
+        for i in range(len(graph)):
+            
+            if colors[i] is None:
+                
+                colors[i] = 1
+                
+                que = [i]
+                
+                while que:
+                    cur_node = que.pop(0)
+                    
+                    for node in graph[cur_node]:
+                        
+                        if colors[node] is None:
+                            colors[node] = 1 - colors[cur_node]
+                            que.append(node)
+                            
+                        elif colors[node] == colors[cur_node]:
+                            return False
+                        
+            return True
+
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        
         n = len(graph)
         self.graph = graph
         self.visited = [False] * n
